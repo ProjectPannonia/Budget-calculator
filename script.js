309a8c305870e6be3c5506e251af04008f5d6fcf
Letenyei Ádám <letenyeidam89@gmail.com>
2021-05-15 14:46:34 +0200

    Update script.js
    Data validator implemented

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
+class UserInterfaceElements {
+    constructor(itemName, itemPrice, itemQuantity) {
+        this.itemName = document.getElementById(itemName);
+        this.itemPrice = document.getElementById(itemPrice);
+        this.itemQuantity = document.getElementById(itemQuantity);
+    }
+}
+class DataContainerElements {
+    constructor(elementCounter, sumOfCost) {
+        this.elementCounter = document.getElementById(elementCounter);
+        this.sumOfCost = document.getElementById(sumOfCost);
+    }
+}
+
+const interfaceElement = new UserInterfaceElements('item_name', 'item_price', 'item_quantity');
+const dataContainers = new DataContainerElements('sum_number_of_items', 'sum_of_items_cost');
 
 let total = 0;
 let numberOfElements = 0;
 
 function save() {
-    const desciption = document.getElementById('item_name');
-    const cost = document.getElementById('item_price');
-    const amount = document.getElementById('item_quantity');
+    let itemName = interfaceElement.itemName.value;
+    let itemPrice = interfaceElement.itemPrice.value;
+    let itemQuantity = interfaceElement.itemQuantity.value;
     
-    addToList(desciption.value,cost.value,amount.value);
+    if(isValidData(itemName, itemPrice, itemQuantity)) {
+        addToList(itemName, itemPrice, itemQuantity);
+    } else {
+        alert('Invalid input data!');
+    }
+    
 }
 
 function addToList(desciption,cost,amount) {
-    let costMultiAmount = parseInt(cost) * parseInt(amount);
+    let multipliedCostAndAmount = parseInt(cost) * parseInt(amount);
+
     addElementToList(desciption, 'db');
-    addElementToList(costMultiAmount + 'Ft');
-
-    total += parseInt(cost);
-    numberOfElements++;
-
-    document.getElementById('sum_number_of_items').innerHTML = ": " + numberOfElements + " db";
-    document.getElementById('sum_of_items_cost').innerHTML = ": " + total.toString() + " Ft";
+    addElementToList( multipliedCostAndAmount + 'Ft');
+    refreshTotals(multipliedCostAndAmount);
 }
 
 function addElementToList(text,unit) {
@@ -28,9 +45,15 @@ function addElementToList(text,unit) {
 
     let listItemNode = document.createElement("li");
     let textNode = document.createTextNode(text);
+    
     listItemNode.appendChild(textNode);
-
     selectedList.appendChild(listItemNode);
+}
+function refreshTotals(multipliedCostAndAmount) {
+    total += parseInt(multipliedCostAndAmount);
+    numberOfElements++;
+    dataContainers.elementCounter.innerHTML = ": " + numberOfElements + " db";
+    dataContainers.sumOfCost.innerHTML = ": " + total.toString() + " Ft";
 }
 /*
 if(typeof desciption.val === 'undefined') {
@@ -38,8 +61,21 @@ if(typeof desciption.val === 'undefined') {
     }
 */
 
-function isEmpty(desciption, cost, amount) {
-    const desc_val = desciption.val;
-    const cost_val = cost.val;
-    const amount_val = amount.val;
+function isValidData(itemNameValue, itemPriceValue, itemQuantityValue) {
+    let validName = isNaN(parseInt(itemNameValue)) && (itemNameValue.length > 0);
+    let validPrice = isValidNumber(itemPriceValue);
+    let validQuantity = isValidNumber(itemQuantityValue);
+
+    return (validName && validPrice && validQuantity);
 }
+
+function isValidNumber(value) {
+    let parsedValue = parseInt(value);
+    let valid = false;
+
+    if(!isNaN(parsedValue) && parsedValue > 0) {
+        valid = true;
+    }
+    return valid;
+}
+
