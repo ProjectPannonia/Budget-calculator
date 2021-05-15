2ec6ce62f77a6327fdb8d09e412cc41e9343278e
Letenyei Ádám <letenyeidam89@gmail.com>
2021-05-15 13:56:31 +0200

    Functionality refactored

--- script.js
+++ script.js
@@ -3,30 +3,34 @@ let total = 0;
 let numberOfElements = 0;
 
 function save() {
-    const desciption = document.getElementById('tetel_neve');
-    const cost = document.getElementById('tetel_ara');
-    const amount = document.getElementById('tetel_db');
+    const desciption = document.getElementById('item_name');
+    const cost = document.getElementById('item_price');
+    const amount = document.getElementById('item_quantity');
     
     addToList(desciption.value,cost.value,amount.value);
-    total += parseInt(cost.value);
-    numberOfElements++;
-    console.log(typeof total);
-    document.getElementById('sum_number_of_items').innerHTML = numberOfElements + " db";
-    document.getElementById('sum_of_items_cost').innerHTML = total.toString() + " Ft";
 }
 
 function addToList(desciption,cost,amount) {
-    let desc_Node = document.createElement("li");
-    let desc_Text = document.createTextNode(desciption);
-    desc_Node.appendChild(desc_Text);
-    document.getElementById('megnevezes_list').appendChild(desc_Node);
-
-    let cost_Node = document.createElement("li");
     let costMultiAmount = parseInt(cost) * parseInt(amount);
-    let cost_Text = document.createTextNode(costMultiAmount + " Ft");
-    cost_Node.appendChild(cost_Text);
-    document.getElementById('koltseg_list').appendChild(cost_Node);
+    addElementToList(desciption, 'db');
+    addElementToList(costMultiAmount + 'Ft');
 
+    total += parseInt(cost);
+    numberOfElements++;
+
+    document.getElementById('sum_number_of_items').innerHTML = ": " + numberOfElements + " db";
+    document.getElementById('sum_of_items_cost').innerHTML = ": " + total.toString() + " Ft";
+}
+
+function addElementToList(text,unit) {
+    let whichList = (unit === 'db' ? 'megnevezes_list' : 'koltseg_list');
+    let selectedList = document.getElementById(whichList);
+
+    let listItemNode = document.createElement("li");
+    let textNode = document.createTextNode(text);
+    listItemNode.appendChild(textNode);
+
+    selectedList.appendChild(listItemNode);
 }
 /*
 if(typeof desciption.val === 'undefined') {
